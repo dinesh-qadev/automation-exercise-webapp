b1f65ef2238205c8aaf146921c656b35d6a1fe3e
dinesh-qadev <dineshjoshi.contact@gmail.com>
2025-05-02 00:03:08 +0545

    Test_010 implemented

--- pageObjects/home_page.py
+++ pageObjects/home_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pageObjects.base_page import BasePage
 
 
@@ -10,6 +12,12 @@ class HomePage(BasePage):
     CONTACT_US_BUTTON = (By.XPATH, "//a[@href='/contact_us']")  # Locator for the 'Contact Us' button
     TEST_CASES_BUTTON = (By.XPATH, "//a[@href='/test_cases']")
     PRODUCTS_BUTTON = (By.XPATH, "//a[@href='/products']")
+
+    #Locators for testcase subscription in home page
+    SUBSCRIPTION_TEXT = (By.XPATH, "//h2[normalize-space()='Subscription']")
+    EMAIL_INPUT = (By.ID, "susbscribe_email")
+    SUBMIT_BUTTON = (By.ID, "subscribe")
+    SUCCESS_MESSAGE = (By.XPATH, "//div[@id='success-subscribe']//div[contains(text(), 'successfully subscribed')]")
 
     def __init__(self, driver):
         #self.driver = driver
@@ -50,3 +58,28 @@ class HomePage(BasePage):
     #Click on product menu
     def click_products(self):
         self.click(self.PRODUCTS_BUTTON)
+
+    #Pageobject Methods for Subscription verification in Homepage
+    def scroll_to_footer(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    def is_subscription_text_visible(self):
+        return self.is_visible(self.SUBSCRIPTION_TEXT)
+
+    def subscribe_with_email(self, email):
+        self.enter_text(self.EMAIL_INPUT, email)
+        self.click(self.SUBMIT_BUTTON)
+        # with open("page_source.html", "w", encoding="utf-8") as f:
+        #     f.write(self.driver.page_source) #This is written to find out alert text from DOM
+
+    def get_success_message_text(self, timeout=5):
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located(self.SUCCESS_MESSAGE)
+            )
+
+            return element.text.strip()
+        except Exception as e:
+            return e
+
+
---
+++ testCases/test_010_subscription_homepage.py
@@ -0,0 +1,24 @@
+# tests/test_subscription_home.py
+import pytest
+from pageObjects.home_page import HomePage
+
+
+def test_subscription_in_home_page(browser):
+    home = HomePage(browser)
+
+    # Step 3: Verify home page is loaded
+    home.is_home_page_displayed()
+
+    # Step 4: Scroll to footer
+    home.scroll_to_footer()
+
+    # Step 5: Verify 'SUBSCRIPTION' text is visible
+    assert home.is_subscription_text_visible(), "'SUBSCRIPTION' text not visible"
+
+    # Step 6: Enter email and click arrow
+    home.subscribe_with_email("testemail@example.com")
+
+    # Step 7: Verify success message
+    message = home.get_success_message_text()
+
+    assert message == "You have been successfully subscribed!", f"Unexpected message: {message}"
